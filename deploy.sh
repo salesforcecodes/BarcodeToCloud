83f10eaf339afffeb9783dd64f0f3466000b7a89
salesforcecodes <forhub@gmail.com>
2021-01-18 22:33:08 -0700

    Adding deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Please enter alias of the environment that you want to deploy to"
+echo "Please enter SFDX alias of the environment that you want to deploy to"
 read aliasInput
 sfdx force:source:deploy -p force-app/main/default  -u $aliasInput
